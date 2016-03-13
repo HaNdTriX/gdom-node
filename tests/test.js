8e1a50e872c46b7b98356c11404e9e20edafce25
lirla@gmail.com <firexable@gmail.com>
2016-03-13 14:49:51 +0300

    Update tests

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,20 @@
-import chai from "chai"
-import chaiAsPromised from "chai-as-promised"
-import fs from "fs"
-import express from "express"
-import {parse} from "../src/gdom-node"
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import fs from 'fs';
+import express from 'express';
+import { parse } from '../src/gdom-node';
 
 chai.should();
 chai.use(chaiAsPromised);
 
 
 const app = express();
-app.get('/', (req,res)=>{
-    res.send(fs.readFileSync("./tests/html/test.html", "utf8"))
+app.get('/', (req, res) => {
+  res.send(fs.readFileSync('./tests/html/test.html', 'utf8'));
 });
 app.listen(3333);
 
-let query = `{
+const query = `{
                 page(url:"http://localhost:3333"){
                     text:text(selector:".text")
                     attr:attr(selector:".attr", name:"href")
@@ -36,59 +36,57 @@ let query = `{
                 }
             }`;
 
-describe("GDOM parser", ()=>{
-    let result;
-    before(()=> {
-        result = parse(query);
-    });
+describe('GDOM parser', () => {
+  let result;
+  before(() => {
+    result = parse(query);
+  });
 
-    describe("text()", ()=>{
-        it("Get text from given selector", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.text', 'text')
-        })
-    });
+  describe('text()', () => {
+    it('Get text from given selector', () =>
+      result.should.eventually.to.have.deep.property('data.page.text', 'text')
+    );
+  });
 
-    describe("attr()", ()=>{
-        it("Get attr from given selector and attr name", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.attr', '/href')
-        })
-    });
+  describe('attr()', () => {
+    it('Get attr from given selector and attr name', () =>
+      result.should.eventually.to.have.deep.property('data.page.attr', '/href')
+    );
+  });
 
-    describe("query()", ()=>{
-        it("Get elements queried by selector", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.items')
-                         .that.is.an('array')
-                         .with.deep.property('[1]')
-                         .that.deep.equals({"text":"4"});
-        })
-    });
+  describe('query()', () => {
+    it('Get elements queried by selector', () =>
+      result.should.eventually.to.have.deep.property('data.page.items')
+        .that.is.an('array')
+        .with.deep.property('[1]')
+        .that.deep.equals({ text: '4' })
+    );
+  });
 
-    describe("html()", ()=>{
-        it("Get element innerHtml", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.itemsHtml')
-                .that.is.an('array')
-                .with.deep.property('[1]')
-                .that.deep.equals({"html":'<span>b</span>'});
-        })
-    });
+  describe('html()', () => {
+    it('Get element innerHtml', () =>
+      result.should.eventually.to.have.deep.property('data.page.itemsHtml')
+        .that.is.an('array')
+        .with.deep.property('[1]')
+        .that.deep.equals({ html: '<span>b</span>' })
+    );
+  });
 
-    describe("tag()",()=>{
-        it("Get element tag", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.tag','a')
-        })
-    });
+  describe('tag()', () => {
+    it('Get element tag', () =>
+      result.should.eventually.to.have.deep.property('data.page.tag', 'a')
+    );
+  });
 
-    describe("next()", ()=>{
-        it("Get next element(sibling)", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.next.text','Next')
-        })
-    });
+  describe('next()', () => {
+    it('Get next element(sibling)', () =>
+      result.should.eventually.to.have.deep.property('data.page.next.text', 'Next')
+    );
+  });
 
-    describe("parent()", ()=>{
-        it("Get element parent", ()=>{
-            return result.should.eventually.to.have.deep.property('data.page.parent[0].parent.text','text')
-        })
-    });
+  describe('parent()', () => {
+    it('Get element parent', () =>
+      result.should.eventually.to.have.deep.property('data.page.parent[0].parent.text', 'text')
+    );
+  });
 });
-
-
